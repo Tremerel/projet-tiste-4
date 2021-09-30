9b829379ec3238ffa2eada7c9eaacf0863988000
tremerel <kevintremerel@hotmail.com>
2021-10-01 00:58:29 +0200

    spacing / clean code

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,17 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    centeredSlides: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      dynamicBullets: true,
-      clickable: true,
-    },
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
 
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-      },
-
-  
-  });
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
